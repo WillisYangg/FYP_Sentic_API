adb7fabe4cb28234207046d42eefe1373f174210
WillisYangg <willisyang7@gmail.com>
2023-10-04 19:39:18 +0800

    Add: subreddits.txt and extract multiple pages

--- reddit_extractor.py
+++ reddit_extractor.py
@@ -1,6 +1,5 @@
 import praw
 import pandas as pd
-import sys
 
 user_agent = "Scrapper 1.0 by /u/willissssa"
 client_id = "7yQTXog0_Zi1dAWu0WkMkg"
@@ -15,15 +14,32 @@ def reddit_api():
     )
     
 def reddit_data_extraction(reddit_page):
-    posts=[]
-    reddit_posts = reddit.subreddit("'"+reddit_page+"'")
+    hot_posts=[]
+    top_posts=[]
+    new_posts=[]
+    column=['title', 'selftext', 'created_utc']
+    reddit_posts = reddit.subreddit(reddit_page)
     for post in reddit_posts.hot(limit=None):
-        posts.append([post.title, post.id, post.author, post.score, post.url, post.selftext, post.created_utc])
-    posts = pd.DataFrame(posts,columns=['title', 'id', 'author', 'score', 'url', 'selftext', 'created_utc'])
-    
-    print(posts)
+        hot_posts.append([post.title, post.selftext, post.created_utc])
+    for post in reddit_posts.top(limit=None):
+        top_posts.append([post.title, post.selftext, post.created_utc])
+    for post in reddit_posts.new(limit=None):
+        new_posts.append([post.title, post.selftext, post.created_utc])
+    hot_posts = pd.DataFrame(hot_posts,columns=column)
+    hot_posts.to_csv('{}_hot_posts.csv'.format(reddit_page))
+    top_posts = pd.DataFrame(top_posts,columns=column)
+    top_posts.to_csv('{}_top_posts.csv'.format(reddit_page))
+    new_posts = pd.DataFrame(new_posts,columns=column)
+    new_posts.to_csv('{}_new_posts.csv'.format(reddit_page))
     
 if __name__ == "__main__":
-    reddit_page = sys.argv[1]
     reddit_api()
-    reddit_data_extraction(reddit_page)
+    file_path = 'subreddits.txt'
+    with open(file_path, 'r') as file:
+        for line in file:
+            print("------------------------------------------------")
+            print("Subreddit: {}".format(line))
+            try:
+                reddit_data_extraction(line)
+            except Exception as e:
+                print("Data Extraction Failed: {}".format(e))
