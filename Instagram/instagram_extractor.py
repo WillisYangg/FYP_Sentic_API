acb990f765354b79f33c503aa4555c13996489d5
WillisYangg <willisyang7@gmail.com>
2023-10-06 16:13:14 +0800

    Add: Instagram scraper

---
+++ Instagram/instagram_extractor.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from getpass import getpass
+from time import sleep
+import pandas as pd
+
