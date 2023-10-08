ab00d6e12f115c042da189459ef6540088752841
WillisYangg <willisyang7@gmail.com>
2023-10-08 17:16:26 +0800

    Add: extract from pages in people (twitter)

--- Twitter/twitter_extractor.py
+++ Twitter/twitter_extractor.py
@@ -8,6 +8,9 @@ import pandas as pd
 import configparser
 import sqlalchemy
 import mysql.connector
+from datetime import date
+
+today = date.today()
 
 PATH = './chromedriver'
 username = 'willissssa'
@@ -40,7 +43,7 @@ def insert_df_to_table(df, table):
                                                       default['DB_IP'], default['DB_DATABASE']))
     df.to_sql(con=engine, name=table, if_exists='append', index=False)
 
-def twitter_data_extraction(search_query, table):
+def twitter_data_extraction_from_hashtags(search_query, table):
     service = Service(executable_path=PATH)
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=service, options=options)
@@ -91,20 +94,85 @@ def twitter_data_extraction(search_query, table):
     driver.close()
 
     df = pd.DataFrame(zip(TimeStamps,Tweets),columns=['TimeStamps','Tweets'])
+    df['Date Scraped'] = today
     df = df.drop_duplicates().reset_index(drop=True)
 
     # df.to_csv("{}_twitter.csv".format(search_query))
     
     insert_df_to_table(df, table)
     
-if __name__ == "__main__":
-    table = 'twitter_data'
-    file_path = 'twitter_search.txt'
+def twitter_data_extraction_from_page(search_query, table):
+    service = Service(executable_path=PATH)
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(target_url)
+    sleep(5)
+
+    driver.find_element("xpath", "//*[text()='Sign in']").click()
+    sleep(5)
+
+    driver.find_element("xpath", "//input[@name='text']").click()
+    driver.find_element("xpath", "//input[@name='text']").send_keys(username)
+    driver.find_element("xpath", "//*[text()='Next']").click()
+    sleep(5)
+
+    driver.find_element("xpath", "//input[@name='password']").click()
+    driver.find_element("xpath", "//input[@name='password']").send_keys(password)
+    driver.find_element("xpath", "//*[text()='Log in']").click()
+    sleep(5)
+
+    driver.find_element("xpath", "//input[@data-testid='SearchBox_Search_Input']").click()
+    driver.find_element("xpath", "//input[@data-testid='SearchBox_Search_Input']").send_keys(search_query)
+    driver.find_element("xpath", "//input[@data-testid='SearchBox_Search_Input']").send_keys(Keys.RETURN)
+    sleep(5)
+    
+    driver.find_element("xpath", "//span[contains(text(),'People')]").click()
+    sleep(5)
+    
+    profile = driver.find_element("xpath", '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[1]/div/div/div/div/div[2]/div[1]/div[1]/div/div[1]/a/div/div[1]/span/span[1]')
+    profile.click()
+    sleep(5)
+    
+    tweets = driver.find_elements("xpath", "//article[@data-testid='tweet']")
+    # sleep(5)
+
+    TimeStamps=[]
+    Tweets=[]
+    
+    while True:
+        for tweet in tweets:
+            TimeStamp = driver.find_element("xpath", ".//time").get_attribute('datetime')
+            TimeStamps.append(TimeStamp)
+            Tweet = driver.find_element("xpath", ".//div[@data-testid='tweetText']").text
+            Tweets.append(Tweet)
+        driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+        sleep(5)
+        tweets = driver.find_elements("xpath", "//article[@data-testid='tweet']")
+        Tweets2 = list(set(Tweets))
+        if len(Tweets2) > 5:
+            break
+        
+    driver.close()
+
+    df = pd.DataFrame(zip(TimeStamps,Tweets),columns=['TimeStamps','Tweets'])
+    df['Date Scraped'] = today
+    df = df.drop_duplicates().reset_index(drop=True)
+    
+    insert_df_to_table(df, table)
+    
+def scrape_from_twitter(file_path, table):
     with open(file_path, 'r') as file:
         for search_query in file:
             print("------------------------------------------------")
             print("Search Query: {}".format(search_query))
             try:
-                twitter_data_extraction(search_query, table)
+                # twitter_data_extraction_from_hashtags(search_query, table)
+                twitter_data_extraction_from_page(search_query, table)
             except Exception as e:
                 print("Data Extraction Failed: {}".format(e))
+    
+if __name__ == "__main__":
+    table = 'twitter_data'
+    file_path = 'twitter_search.txt'
+    scrape_from_twitter(file_path, table)
